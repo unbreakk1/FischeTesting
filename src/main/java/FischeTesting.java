35b2ae0381322e3c5517167fa06df2bf4fa3c59d
unbreakk1 <95477818+unbreakk1@users.noreply.github.com>
2025-01-29 15:41:25 +0100

    refactor output

--- src/main/java/FischeTesting.java
+++ src/main/java/FischeTesting.java
@@ -7,11 +7,13 @@ public class FischeTesting
         int sum = add(a, b);
         int product = multiply(a, b);
         String string = "hello world";
+
         System.out.println("Summe: " + sum);
+        System.out.println("Is Even: " + checkIsEven(a));
         System.out.println("Product: " + product);
         System.out.println("Original String: " + string);
-        System.out.println("String: " + convertString(string));
-        System.out.println("Is Even: " + checkIsEven(a));
+        System.out.println("Uppercase String: " + convertString(string));
+        System.out.println("Is Positive: " + checkIsPositive(a));
     }
 
     public static int add(int a, int b)
